a7f0956c77260b0bf9e39cacdb16b5c4a7ab9525
smerante <merante.s@hotmail.com>
2022-01-31 16:02:07 -0500

    intial expo setup with deps

---
+++ WhoPaysTheBills/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Test</Text>
+      <StatusBar style="auto" />
+      <Button title='Click Me' onPress={()=>alert("Clicked")}></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
