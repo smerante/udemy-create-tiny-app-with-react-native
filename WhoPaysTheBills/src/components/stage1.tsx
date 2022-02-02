45e72aa71733f5dd31df38e4841aaf01bc33113b
smerante <merante.s@hotmail.com>
2022-02-01 19:30:59 -0500

    the input layer

--- WhoPaysTheBills/src/components/stage1.tsx
+++ WhoPaysTheBills/src/components/stage1.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Input, Button, ListItem, Text } from 'react-native-elements';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { MyContext } from '../context';
 
 const StageOne = () => {
-    return  <View style={styles.container}>
-      <Text>Stage 1</Text>
-    </View>
+    const context = useContext(MyContext);
+    const minNameLength = 3;
+    const maxNameLength = 3;
+    return (
+        <Formik
+            initialValues={{ player: '' }}
+            validationSchema={Yup.object({
+                player: Yup.string()
+                    .min(minNameLength, `Must be more than ${minNameLength} characters`)
+                    .max(maxNameLength, `Max amount of characters is ${maxNameLength}`)
+                    .required('Name is required'),
+            })}
+            onSubmit={(values, { resetForm }) => { alert(JSON.stringify(values)) }}>
+            {({ handleChange, handleBlur, handleSubmit, values, touched, errors }) => (
+                <>
+                    <Text>Who Pays The Bill?</Text>
+                    <Input
+                        placeholder='Add name'
+                        leftIcon={{ type: 'antdesign', name: 'adduser' }}
+                        inputContainerStyle={{
+                            marginHorizontal: 50,
+                            marginTop: 50,
+                        }}
+                        onChangeText={handleChange('player')}
+                        onBlur={handleBlur('player')}
+                        value={values.player}
+                    />
+                    <Button 
+                        title='Add Player'
+                        onPress={() => handleSubmit()}
+                    />
+                </>
+            )}
+        </Formik>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
 
 export default StageOne;
